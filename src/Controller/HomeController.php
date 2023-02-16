ef9783216d6de995db2840a0d88a6fecd4472c78
NICOLAU Bastien <bastien.nicolau@univ-amu.fr>
2023-02-16 16:00:58 +0100

    - Bugfix pour l'accueil (route='/') avec la translation.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,17 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route(
-    path: '/{_locale}/'
-)]
+
 class HomeController extends AbstractController
 {
     #[Route(
-        path: '',
+        path: '/',
         name: 'app_index',
-        requirements: ['_locale' => 'fr|en'],
     )]
     public function index(): Response
+    {
+        // Si on ne fait pas ça, avec la translation, on arrive pas sur homepage.
+        return $this->redirectToRoute('app_homepage');
+    }
+
+    #[Route(
+        path: '/{_locale}/',
+        name: 'app_homepage',
+        requirements: ['_locale' => 'fr|en'],
+    )]
+    public function homepage(): Response
     {
         return $this->render('home/index.html.twig');
     }
